ded2b8efb8e9cd0e69c9f1fec42b0c55050bf928
shawnw <shawnw@speakeasy.org>
2016-01-04 12:29:04 -0800

    Add range.hpp
    rev(), take(), drop() range based for loop adaptors.

---
+++ tests/range.cc
@@ -0,0 +1,68 @@
+#include <vector>
+#include <iostream>
+#include "useful/range.hpp"
+
+using namespace useful;
+
+int main(void) {
+	const std::vector<int> cfoo{1,2,3,4,5};
+	std::vector<int> bar{6,7,8,9,10};	
+	
+	std::cout << "Original:\n";
+	for (auto i : cfoo)
+		std::cout << i << ',';
+	std::cout << '\n';
+	for (auto i : bar)
+		std::cout << i << ',';
+	std::cout << '\n';
+		
+	
+	std::cout << "\nReversed:\n";
+	for (auto i : rev(cfoo))
+		std::cout << i << ',';
+	std::cout << '\n';
+	for (auto i : rev(bar))
+		std::cout << i << ',';
+	std::cout << '\n';
+	
+	std::cout << "\nDrop 2:\n";
+	for (auto i : drop(cfoo, 2))
+		std::cout << i << ',';
+	std::cout << '\n';
+	for (auto i : drop(bar, 2)) 
+		std::cout << i << ',';
+	std::cout << '\n';
+		
+	std::cout << "\nTake 2:\n";
+	for (auto i : take(cfoo, 2))
+		std::cout << i << ',';
+	std::cout << '\n';
+	for (auto i : take(bar, 2))
+		std::cout << i << ',';
+	std::cout << '\n';
+	for (const auto &i : take(bar, 2))
+		std::cout << i << ',';
+	std::cout << '\n';
+	
+	std::cout << "\nTake -2:\n";
+	for (auto i : take(cfoo, -2))
+		std::cout << i << ',';
+	std::cout << '\n';
+	for (auto i : take(bar, -2))
+		std::cout << i << ',';
+	std::cout << '\n';
+	
+	std::cout << "\nPretty:\n";
+	for (auto i : cfoo) 
+		std::cout << i << ", ";
+	for (auto i : take(bar, -1))
+		std::cout << i << ", ";
+	std::cout << bar.back() << '\n';
+	
+	return 0;
+}
+	
+	
+	
+	
+	
---
+++ useful/range.hpp
@@ -0,0 +1,253 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2016 shawnw
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include <iterator>
+#include <type_traits>
+
+namespace useful {
+
+	/* Adaptors for range-based for loops to iterate over a container in reverse
+	 * order. Requires a container to provide rbegin() and rend().
+	 * const_reverse_adaptor is for immutable containers, and reverse_adaptor for
+	 * mutable ones. The rev() function automatically picks the right one to use
+	 * based on the const-ness of its argument.
+	 *
+	 *
+	 * | const std::vector<int> v;
+	 * | for (auto &i : rev(v)) {}
+	 * | for (auto &i : const_reverse_adaptor<std::vector<int>>(v)) {}
+	 * 
+	 * are equivalent.
+	 */
+	template<typename Container>
+	class const_reverse_adaptor {
+	public:
+		using container_type = typename std::add_const<Container>::type;
+		using const_iterator = typename Container::const_reverse_iterator;
+	
+	private:
+		container_type &c;
+	
+	public:
+		explicit const_reverse_adaptor(container_type &c_)
+			: c(c_) {}
+	
+		const_iterator begin(void) const noexcept { return c.rbegin(); }
+		const_iterator cbegin(void) const noexcept { return c.rbegin(); }
+		
+		const_iterator end(void) const noexcept { return c.rend(); }
+		const_iterator cend(void) const noexcept { return c.rend(); }
+	};
+	
+	template<typename Container>
+	class reverse_adaptor {
+	public:
+		using container_type = typename std::remove_const<Container>::type;
+		using iterator = typename Container::reverse_iterator;
+		using const_iterator = typename Container::const_reverse_iterator;
+	
+	private:
+		container_type &c;
+	
+	public:
+		explicit reverse_adaptor(container_type &c_)
+			: c(c_) {}
+	
+		iterator begin(void) noexcept { return c.rbegin(); }
+		const_iterator begin(void) const noexcept { return c.rbegin(); }
+		const_iterator cbegin(void) const noexcept { return c.rbegin(); }
+		
+		iterator end(void) noexcept { return c.rend(); }
+		const_iterator end(void) const noexcept { return c.rend(); }
+		const_iterator cend(void) const noexcept { return c.rend(); }
+	};
+	
+	template<typename Container,
+		class Const = typename std::enable_if<std::is_const<Container>::value>::type>
+	const_reverse_adaptor<Container> rev(Container &c) {
+		return const_reverse_adaptor<Container>(c);
+	}
+	
+	template<typename Container,
+		class NotConst = typename std::enable_if<!std::is_const<Container>::value>::type>
+	reverse_adaptor<Container> rev(Container &c) {
+		return reverse_adaptor<Container>(c);
+	} 
+
+	/* Adaptors to iterate over all but the first N elements of a container.
+	 * for (auto i : drop(foo, 5)) skips the first five elements.
+	*/
+	template<typename Container>
+	class const_drop_adaptor {
+	public:
+		using container_type = typename std::add_const<Container>::type;
+		using const_iterator = typename Container::const_iterator;
+		using difference_type = typename Container::difference_type;
+		
+	private:
+		container_type &c;
+		difference_type n;
+		
+	public:
+		explicit const_drop_adaptor(container_type &c_, difference_type n_)
+			: c(c_), n(n_) {}
+	
+		const_iterator begin(void) const noexcept {
+			auto i = c.cbegin();
+			std::advance(i, n);
+			return i;
+		}
+		const_iterator cbegin(void) const noexcept {
+			auto i = c.cbegin();
+			std::advance(i, n);
+			return i;
+		}
+		
+		const_iterator end(void) const noexcept { return c.cend(); }
+		const_iterator cend(void) const noexcept { return c.cend(); }
+	};
+	
+	template<typename Container>
+	class drop_adaptor {
+	public:
+		using container_type = typename std::remove_const<Container>::type;
+		using iterator = typename Container::iterator;
+		using const_iterator = typename Container::const_iterator;
+		using difference_type = typename Container::difference_type;
+		
+	private:
+		container_type &c;
+		difference_type n;
+	
+	public:
+		explicit drop_adaptor(container_type &c_, difference_type n_)
+			: c(c_), n(n_) {}
+	
+		iterator begin(void) noexcept {
+			auto i = c.begin();
+			std::advance(i, n);
+			return i;
+		}
+		const_iterator begin(void) const noexcept {
+			auto i = c.cbegin();
+			std::advance(i, n);
+			return i;
+		}
+		const_iterator cbegin(void) const noexcept {
+			auto i = c.cbegin();
+			std::advance(i, n);
+			return i;
+		}
+		
+		iterator end(void) noexcept { return c.end(); }
+		const_iterator end(void) const noexcept { return c.cend(); }
+		const_iterator cend(void) const noexcept { return c.cend(); }
+	};
+	
+	template<typename Container,
+		class Const = typename std::enable_if<std::is_const<Container>::value>::type>
+	const_drop_adaptor<Container> drop(Container &c, typename Container::difference_type n) {
+		return const_drop_adaptor<Container>(c, n);
+	}
+	
+	template<typename Container,
+		class NotConst = typename std::enable_if<!std::is_const<Container>::value>::type>
+	drop_adaptor<Container> drop(Container &c, typename Container::difference_type n) {
+		return drop_adaptor<Container>(c, n);
+	} 
+	
+	/* Adaptors to iterate over just the first N elements of a container.
+	 *  for (auto i : take(foo, 5)) stops after five elements.
+	 * If N is negative, stops abs(N) elements from the end. So
+	 *  for (auto i : take(foo, -1)) iterates over all but the last element.
+	*/
+	template<typename Container>
+	class const_take_adaptor {
+	public:
+		using container_type = typename std::add_const<Container>::type;
+		using const_iterator = typename Container::const_iterator;
+		using difference_type = typename Container::difference_type;
+		
+	private:
+		container_type &c;
+		const_iterator ce;
+		
+	public:
+		explicit const_take_adaptor(container_type &c_, difference_type n_)
+			: c(c_), ce(c.cbegin()) { 
+				if (n_ < 0)
+					n_ = c.size() + n_;
+				std::advance(ce, n_);
+			}
+	
+		const_iterator begin(void) const noexcept { return c.cbegin(); }
+		const_iterator cbegin(void) const noexcept { return c.cbegin(); }
+		
+		const_iterator end(void) const noexcept { return ce; }
+		const_iterator cend(void) const noexcept { return ce; }
+	};
+	
+	template<typename Container>
+	class take_adaptor {
+	public:
+		using container_type = typename std::remove_const<Container>::type;
+		using iterator = typename Container::iterator;
+		using const_iterator = typename Container::const_iterator;
+		using difference_type = typename Container::difference_type;
+		
+	private:
+		container_type &c;
+		iterator e;
+		const_iterator ce;
+		
+	public:
+		explicit take_adaptor(container_type &c_, difference_type n_)
+			: c(c_), e(c.begin()), ce(c.cbegin()) {
+				if (n_ < 0) 
+					n_ = c.size() + n_;
+				std::advance(e, n_);
+				std::advance(ce, n_);
+			}
+	
+		iterator begin(void) noexcept { return c.begin(); }
+		const_iterator begin(void) const noexcept { return c.cbegin(); }
+		const_iterator cbegin(void) const noexcept { return c.cbegin(); }
+		
+		iterator end(void) noexcept { return e; }
+		const_iterator end(void) const noexcept { return ce; }
+		const_iterator cend(void) const noexcept { return ce; }
+	};
+	
+	template<typename Container,
+		class Const = typename std::enable_if<std::is_const<Container>::value>::type>
+	const_take_adaptor<Container> take(Container &c, typename Container::difference_type n) {
+		return const_take_adaptor<Container>(c, n);
+	}
+	
+	template<typename Container,
+		class NotConst = typename std::enable_if<!std::is_const<Container>::value>::type>
+	take_adaptor<Container> take(Container &c, typename Container::difference_type n) {
+		return take_adaptor<Container>(c, n);
+	} 
+};
