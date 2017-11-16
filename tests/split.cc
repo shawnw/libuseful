9056179bf4a8532bd91cdb2c1b89789c7a78436a
Shawn <shawnw.mobile@gmail.com>
2017-11-16 04:06:31 -0800

    Add a strok() like tokenize() function.

--- tests/split.cc
+++ tests/split.cc
@@ -5,7 +5,7 @@
 #include "useful/string.hpp"
 
 int main(void) {
-  std::string test1 = "this is   a test\tstring to split up.";
+  std::string test1 = "this is\t   a test\tstring to\tsplit up.";
   //std::vector<std::string> results;
   
   int words = useful::split(test1, std::ostream_iterator<std::string>(std::cout, "\n"));
@@ -14,9 +14,14 @@ int main(void) {
   
   std::string test2 = "this is the string the test is done on.";
   auto wordv = useful::splitv(test2, "the");
-  
+
   for (auto &w : wordv)
   	std::cout << w << '\n';
-  
+
+  wordv = useful::tokenize(test1, "\t");
+
+  for (auto &w : wordv)
+    std::cout << w << '\n';
+
   return 0;
 } 
--- useful/string.hpp
+++ useful/string.hpp
@@ -29,46 +29,58 @@ SOFTWARE.
 #include <regex>
 
 namespace useful {
-	
-	// perl style split on regular expression functions.
-	template<class OutputIterator>
-	int split(const std::string &s, const std::regex &re, OutputIterator o) {
-	 std::regex_token_iterator<std::string::const_iterator> rend{},
-		ri{s.begin(), s.end(), re, -1};
-	 int n = 0;
-	 while (ri != rend) {
-		 *o++ = *ri++;
-		 n += 1;
-	 }
-	 return n;
-	}
-	
-	template<class OutputIterator, class T>
-	int split(const std::string &s, const T &re, OutputIterator o) {
-		return split(s, std::regex(re), o);
-	}
-	
-	template<class OutputIterator>
-	int split(const std::string &s, OutputIterator o) {
-		const static std::regex whitespace{"\\s+"};
-	 return split(s, whitespace, o);
-	}
-	
-	std::vector<std::string> splitv(const std::string &s, const std::regex &re) {
-		std::regex_token_iterator<std::string::const_iterator> rend{},
-			ri{s.begin(), s.end(), re, -1};
-		return std::vector<std::string>(ri, rend);
-	}
-	
-	template<class T>
-	std::vector<std::string> splitv(const std::string &s, const T &re) {
-		return splitv(s, std::regex(re));
-	}
-	
-	std::vector<std::string> splitv(const std::string &s) {
-		const static std::regex whitespace{"\\s+"};
-	 return splitv(s, whitespace);
-	}
+
+  // perl style split on regular expression functions.
+  template<class OutputIterator>
+  int split(const std::string &s, const std::regex &re, OutputIterator o) {
+    std::regex_token_iterator<std::string::const_iterator> rend{},
+      ri{s.begin(), s.end(), re, -1};
+      int n = 0;
+      while (ri != rend) {
+        *o++ = *ri++;
+        n += 1;
+      }
+      return n;
+  }
+
+  template<class OutputIterator, class T>
+  int split(const std::string &s, const T &re, OutputIterator o) {
+    return split(s, std::regex(re), o);
+  }
+
+  template<class OutputIterator>
+  int split(const std::string &s, OutputIterator o) {
+    const static std::regex whitespace{"\\s+"};
+    return split(s, whitespace, o);
+  }
+
+  inline std::vector<std::string> splitv(const std::string &s, const std::regex &re) {
+    std::regex_token_iterator<std::string::const_iterator> rend{},
+      ri{s.begin(), s.end(), re, -1};
+      return std::vector<std::string>(ri, rend);
+  }
+
+  template<class T>
+  std::vector<std::string> splitv(const std::string &s, const T &re) {
+    return splitv(s, std::regex(re));
+  }
+
+  inline std::vector<std::string> splitv(const std::string &s) {
+    const static std::regex whitespace{"\\s+"};
+    return splitv(s, whitespace);
+  }
+
+  /* And a strtok style tokenizing function */
+  inline std::vector<std::string> tokenize(const std::string &s, const std::string &tokens) {
+    std::string::size_type start{0}, end;
+    std::vector<std::string> res;
+    while ((end = s.find_first_of(tokens, start)) != std::string::npos) {
+      res.push_back(std::move(s.substr(start, end - start)));
+      start = end + 1;
+    }
+    res.push_back(std::move(s.substr(start)));
+    return res;
+  }
 };
 
 #endif
